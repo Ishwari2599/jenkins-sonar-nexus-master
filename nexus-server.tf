766fef6c0bbc275d173f75593dcfa7682b70c97d
Ishwari_Tajane <ishwaritajane@gmail.com>
2026-04-14 13:15:58 +0530

    Update nexus-server.tf

--- nexus-server.tf
+++ nexus-server.tf
@@ -13,7 +13,7 @@
 # }
 
 
-resource "aws_instance" "nexus-server" {   # we are creating a new instance for jenkins-server
+resource "aws_instance" "nexus-server" {   # we are creating a new instance for nexus-server
     ami = data.aws_ami.latest.id    # we are using the latest ami that we fetched earlier
     instance_type = "t3.medium"     # This is the type of the instance we are creating
     subnet_id = aws_subnet.our-public-subnet.id   # this is the id of the subnet we are using to launch the instance
